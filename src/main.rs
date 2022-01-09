0a35207457d697586fe86a190f1bd6fcc969edbe
hi.p <hi.p@outlook.com>
2022-01-09 22:59:02 +0530

    inital testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    println!("another println");
+    print!("nvim with coc");
+    another_fun();
+    another_fun_fun();
+}
+
+
+fn another_fun() {
+   println!("ntp"); 
+}
+
+fn another_fun_fun() {
+    println!("lts");
 }
